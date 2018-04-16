9f091c7b5627ab4205829cc24366559f1fa84501
Rudra <37140269+rudrathegreat@users.noreply.github.com>
2018-04-16 22:16:56 +1000

    Add files via upload

--- Code/RPS.py
+++ Code/RPS.py
@@ -7,43 +7,40 @@ class RPS:
         display.scroll('You Lose')
         display.show(Image.SAD)
         sleep(1000)
-        pass
         
     def win():
         display.scroll('You Win')
         display.show(Image.HAPPY)
         sleep(1000)
-        pass
         
     def draw():
         display.scroll('Draw')
         display.show(Image.MEH)
         sleep(1000)
-        pass
         
     def computer_decides(choice = None):
         
         options = ['rock', 'paper', 'scissors']
+        number = random.randint(0,2)
+        computer = options[number]
+        display.scroll('Computer Chose ' + computer)
+        sleep(1000)
         
         if choice == 'rock':
             
             for i in range(1):
                 
-                number = random.randint(0,2)
-                computer = options[number]
                 if computer == 'paper':
                     RPS.lose()
                 elif computer == 'scissors':
                     RPS.win()
                 else:
                     RPS.draw()
-                pass
+                
         if choice == 'scissors':
             
             for i in range(1):
                 
-                number = random.randint(0,2)
-                computer = options[number]
                 if computer == 'rock':
                     RPS.lose()
                 elif computer == 'paper':
@@ -51,12 +48,11 @@ class RPS:
                 else:
                     RPS.draw()
                 pass
+                
         if choice == 'paper':
             
             for i in range(1):
                 
-                number = random.randint(0,2)
-                computer = options[number]
                 if computer == 'scissors':
                     RPS.lose()
                 elif computer == 'rock':
@@ -64,65 +60,68 @@ class RPS:
                 else:
                     RPS.draw()
                 pass
-    def quit(variable = None):
-        while variable != 1:
-            display.show('Q')
+                
+    def quit():
+        display.show('Q')
+        sleep(2000)
+        display.off()
+        while True:
             if button_a.is_pressed():
-                variable = 1
+                RPS.game()
+    
+    def turn_display_on():
+        if display.is_on():
+            display.clear()
+        else:
+            display.on()
 
     def game():
+        sleep(1000)
+        RPS.turn_display_on()
+        display.show(Image.HAPPY)
         
-        variable = 0
-
         while True:
             if button_b.is_pressed():
-                RPS.quit(variable)
-            if button_a.is_pressed():
-                variable = 1
-                display.clear()
-                display.show(Image.SMILE)
+                RPS.quit()
+            elif button_a.is_pressed():
+                RPS.turn_display_on()
+                display.scroll('Start')
                 
                 scissors = Image('99009:99090:00900:99090:99009')
                 paper = Image('09990:09990:09990:09990:09990')
                 rock = Image('00000:09990:09990:09990:00000')
-                
-                if button_b.is_pressed():
-                    RPS.quit()
-                
-                sleep(1000)
                 while True:           
-                    display.clear()
                     if accelerometer.was_gesture('left'):
                         display.show(rock)
                         for i in range(1000):
-                            if accelerometer.was_gesture('right'):
+                            # Confirming from the user
+                            if accelerometer.was_gesture('shake'):
                                 display.scroll('Rock')
                                 sleep(1000)
                                 RPS.computer_decides('rock')
                                 RPS.game()
-                                pass
                             sleep(10)
-                    if accelerometer.was_gesture('up'):
+                    elif accelerometer.was_gesture('up'):
                         display.show(scissors)
                         for i in range(1000):
-                            if accelerometer.was_gesture('right'):
+                            # Confirming from the user
+                            if accelerometer.was_gesture('shake'):
                                 display.scroll('Scissors')
                                 sleep(1000)
                                 RPS.computer_decides('scissors')
                                 RPS.game()
-                                pass
                             sleep(10)
-                    if accelerometer.was_gesture('shake'):
+                    elif accelerometer.was_gesture('right'):
+                        # Confirming from the user
                         display.show(paper)
-                        while True:
-                            if accelerometer.was_gesture('right'):
+                        for i in range(1000):
+                            if accelerometer.was_gesture('shake'):
                                 display.scroll('Paper')
                                 sleep(1000)
                                 RPS.computer_decides('paper')
                                 RPS.game()
-                                pass
                             sleep(10)
-                    if button_b.is_pressed():
+                    elif button_b.is_pressed():
                         RPS.quit()
                         
 RPS.game()
